6145bc354e60ef5948c2a71c8d0d2433deb2fec5
Wellington Ideão <wellington.ideao@gmail.com>
2022-08-01 18:15:58 -0300

    Added All CRUD endpoints for lembretes

--- api/views.py
+++ api/views.py
@@ -1,8 +1,8 @@
-from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.viewsets import ModelViewSet
 from .models import Lembrete
 from .serializers import LembreteSerializer
 
 
-class LembreteViewSet(ReadOnlyModelViewSet):
+class LembreteViewSet(ModelViewSet):
     queryset = Lembrete.objects.all()
     serializer_class = LembreteSerializer
