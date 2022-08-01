f572d191cec01746f1ec060eb836e9c579dea05f
Wellington Ideão <wellington.ideao@gmail.com>
2022-07-31 23:23:25 -0300

    Created Lembrete model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.utils.translation import gettext_lazy as _
 # Create your models here.
+
+
+class Lembrete(models.Model):
+    class PrioridadeChoices(models.TextChoices):
+        ALTA = 'ALTA', _('Alta')
+        MEDIA = 'MEDIA', _('Media')
+        BAIXA = 'BAIXA', _('Baixa')
+
+    conteudo = models.CharField(max_length=100)
+    arquivado = models.BooleanField(default=False)
+    prioridade = models.CharField(max_length=5, choices=PrioridadeChoices.choices,
+                                  default=PrioridadeChoices.BAIXA)
+    modificado = models.IntegerField()
+
